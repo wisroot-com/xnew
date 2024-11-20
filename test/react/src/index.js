d69a389d59bae45d0b9618b2a4ab6f66cb39714a
wisroot-com <hisayoshi.furihata@wisroot.com>
2024-11-20 22:01:42 +0900

    update

---
+++ test/react/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Main/>);
+
+function Main() {
+    return (
+        <h1>Hello, world!</h1>
+    );
+}
