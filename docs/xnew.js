ef3e052c7225ad9a5fc43395c7fc320dda415fc3
wisroot-com <hisayoshi.furihata@wisroot.com>
2024-11-22 22:08:05 +0900

    update

--- docs/xnew.js
+++ docs/xnew.js
@@ -1,5 +1,25 @@
-function xnew(...args) {
-    const e = document.createElement('div');
-    e.innerText = 'aaa';
-    document.body.appendChild(e);
-}
+
+   
+    function xnew(...args) {
+        const e = document.createElement('div');
+        e.innerText = 'aaa';
+        document.body.appendChild(e);
+        // // a parent xnode
+        // let parent = undefined;
+        // if (args[0] instanceof XNode || args[0] === null || args[0] === undefined) {
+        //     parent = args.shift();
+        // }
+
+
+        // let element = undefined;
+        // if (args[0] instanceof Element || isObject(args[0]) || args[0] === null || args[0] === undefined) {
+        //     element = args.shift();
+        // }
+
+        // // Component function (+args), or innerHTML
+        // const content = args;
+
+        // return new XNode(parent, element, ...content);
+    }
+
+ 
