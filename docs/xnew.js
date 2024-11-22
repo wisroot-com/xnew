6fa7aad95f17ba2b921100a99d6a717956408f66
wisroot-com <hisayoshi.furihata@wisroot.com>
2024-11-22 22:23:54 +0900

    update

--- docs/xnew.js
+++ docs/xnew.js
@@ -1,8 +1,8 @@
-(function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-    typeof define === 'function' && define.amd ? define(['exports'], factory) :
-    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.window = global.window || {}));
-})(this, (function (exports) { 'use strict';
+// (function (global, factory) {
+//     typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+//     typeof define === 'function' && define.amd ? define(['exports'], factory) :
+//     (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.window = global.window || {}));
+// })(this, (function (exports) { 'use strict';
 
    
     function xnew(...args) {
@@ -28,6 +28,6 @@
     }
 
 
-    exports.xnew = xnew;
+//     exports.xnew = xnew;
 
-}));
+// }));
