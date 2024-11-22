db00797cd158c278c695e80687163248ed5b62a4
wisroot-com <hisayoshi.furihata@wisroot.com>
2024-11-22 22:58:24 +0900

    update

--- src/core/xnew.js
+++ src/core/xnew.js
@@ -2,23 +2,19 @@ import { isObject } from './common';
 import { XNode } from './xnode';
 
 export function xnew(...args) {
-    const e = document.createElement('div');
-    e.innerText = 'aaa';
-    document.body.appendChild(e);
-    // a parent xnode
+
+    // parent xnode
     let parent = undefined;
     if (args[0] instanceof XNode || args[0] === null || args[0] === undefined) {
         parent = args.shift();
     }
 
-
+    // base element
     let element = undefined;
     if (args[0] instanceof Element || isObject(args[0]) || args[0] === null || args[0] === undefined) {
         element = args.shift();
     }
 
     // Component function (+args), or innerHTML
-    const content = args;
-
-    return new XNode(parent, element, ...content);
+    return new XNode(parent, element, ...args);
 }
