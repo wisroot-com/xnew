b791d290eb5acf62428a2610170e0ba2743f7509
wisroot-com <hisayoshi.furihata@wisroot.com>
2024-11-22 20:12:11 +0900

    update

--- src/core/xnode.js
+++ src/core/xnode.js
@@ -242,19 +242,21 @@ export class XNode {
     }
 
     _start() {
-        if (this._.state === 'stopped' && (this._.parent === null || this._.parent.state === 'running') && this._.resolve === true && this._.tostart === true) {
-            this._.startTime = XNode.updateTime;
-            this._.state = 'running';
-            this._.children.forEach((node) => node._start());
-        
-            if (this._.state === 'running' && isFunction(this._.defines.start)) {
-                XNode.wrap(this, this._.defines.start);
+        if (['pending', 'stopped'].includes(this._.state)) {
+            if ((this._.parent === null || this._.parent.state === 'running') && this._.resolve === true && this._.tostart === true) {
+                this._.startTime = XNode.updateTime;
+                this._.state = 'running';
+                this._.children.forEach((node) => node._start());
+            
+                if (this._.state === 'running' && isFunction(this._.defines.start)) {
+                    XNode.wrap(this, this._.defines.start);
+                }
             }
         }
     }
 
     _stop() {
-        if (this._.state === 'running') {
+        if (['running'].includes(this._.state)) {
             this._.state = 'stopped';
             this._.children.forEach((node) => node._stop());
 
@@ -265,8 +267,7 @@ export class XNode {
     }
 
     _update() {
-
-        if (this._.state === 'running' || this._.state === 'stopped') {
+        if (['pending', 'running', 'stopped'].includes(this._.state)) {
             if (this._.tostart === true) this._start();
 
             this._.children.forEach((node) => node._update());
@@ -280,7 +281,7 @@ export class XNode {
     finalize() {
         this._stop();
 
-        if (this._.state === 'stopped') {
+        if (['pending', 'stopped'].includes(this._.state)) {
             this._finalize();
             
             // relation
