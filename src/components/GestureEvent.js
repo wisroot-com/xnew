6c69beddd452e0b06d0f3b956581f81b1ae6348e
wisroot-com <hisayoshi.furihata@wisroot.com>
2024-12-13 01:26:42 +0900

    update

--- src/components/GestureEvent.js
+++ src/components/GestureEvent.js
@@ -23,7 +23,7 @@ export function GestureEvent(xnode) {
             const a = map.get(id);
             map.delete(id);
             const b = [...map.values()][0]; 
-            
+
             const v = { x: a.x - b.x, y: a.y - b.y };
             const s =  v.x * v.x + v.y * v.y;
             const scale = 1 + (s > 0.0 ? (v.x * delta.x + v.y * delta.y) / s : 0);
@@ -40,10 +40,4 @@ export function GestureEvent(xnode) {
         active = false;
         map.delete(id);
     });
-
-    return {
-        get active() {
-            return active;
-        }
-    }
 }
